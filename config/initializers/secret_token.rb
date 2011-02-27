82763dd2bceadcd01b4c596a6cc21e6f869ae9b5
Jenny Rasmussen <jenny@jrr-ubuntu.(none)>
2011-02-27 15:29:36 -0800

    Baseline with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rasmusj2Reddit::Application.config.secret_token = 'f9c2fd08d0c3098490641cc394ba204ac01e77a392c16d2519b4983daa91d1ce8372b3ebd13881b9a75007fddb182f160eef3d4fe0bacf44facd0fa210c433e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rasmusj2Reddit::Application.config.session_store :cookie_store, :key => '_rasmusj2_reddit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rasmusj2Reddit::Application.config.session_store :active_record_store
