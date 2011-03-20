70ae8a592074d80ad495eee5b413711c1acfb71e
rasmusj2 <rasmusj2@hotmail.com>
2011-03-20 11:12:00 -0700

    Begin testing - modify unit/link_test.rb

--- test/unit/link_test.rb
+++ test/unit/link_test.rb
@@ -1,8 +1,13 @@
-require 'test_helper'
+require 'test_helper.rb'
 
 class LinkTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
+  end
+
+  test "new link requires title and url" do
+    link = Link.new
+    assert !link.save
   end
 end
